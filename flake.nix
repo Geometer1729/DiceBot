19d92a6c8e21cb0a110aef318a70254223d760b2
Geometer1729 <16kuhnb@gmail.com>
2023-04-26 20:38:31 -0400

    update inputs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "dice-bot";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
@@ -10,41 +11,19 @@
       url = "github:discord-haskell/discord-haskell";
       flake = false;
     };
-    recursion-schemes = {
-      url = "github:recursion-schemes/recursion-schemes";
-      flake = false;
-    };
-    base64-bytestring = {
-      url = "github:haskell/base64-bytestring?rev=6fbb78226a3a00174325ff9bbd98248fb3eb1130";
-      flake = false;
-    };
-    wuss = {
-      url = "github:tfausak/wuss?rev=7cab441d443b3a829cd7e04842aff25388b8f4ef";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-parts, haskell-flake, treefmt-flake, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs@{ flake-parts, haskell-flake, treefmt-flake, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
       imports = [
         haskell-flake.flakeModule
         treefmt-flake.flakeModule
       ];
       perSystem = { self', config, pkgs, ... }: {
         haskellProjects.default = {
-          root = ./.;
-          buildTools = hp: {
-            inherit (pkgs)
-              treefmt;
-          } // config.treefmt.formatters;
-          source-overrides = {
-            inherit (self.inputs)
-              recursion-schemes
-              discord-haskell
-              base64-bytestring
-              wuss
-              ;
+          overrides = self: super: with pkgs.haskell.lib; {
+            discord-haskell = dontCheck (self.callCabal2nix "discord-haskell" inputs.discord-haskell {});
           };
         };
         treefmt.formatters = {
